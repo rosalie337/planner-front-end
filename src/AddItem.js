dc71e9d3838764add8e9325ec3949cb909704563
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-02-27 12:04:40 -0800

    added AddItem.js

---
+++ src/AddItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class AddTodo extends Component {
+    render() {
+        return (
+            <div>
+                <input value={ this.props.todoInput } onChange={ this.props.handleInput} />
+                <button onClick={this.props.handleClick}>Add</button>
+            </div>
+        )
+    }
+}
